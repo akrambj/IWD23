4f60a4def9f5afc6b7d1183a553163f9e73fc4e4
akrambj <bedjaouiakram.93@gmail.com>
2023-03-12 21:57:32 +0000

    Structuring react project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Landing from './pages/landing'
+
+
+const App = () => {
+  return (
+    <>
+    <Landing />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import Hero from '../components/hero/Hero'
+
+const Landing = () => {
+  return (
+    <div className='text-center'>
+        <Header />
+        <Hero />
+    </div>
+  )
+}
+
+export default Landing
